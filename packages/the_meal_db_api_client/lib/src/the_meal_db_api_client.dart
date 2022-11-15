7e10b05efa7e823b81daaf804b2cc24fcce0b559
Ben-vgv <117770244+Ben-vgv@users.noreply.github.com>
2022-11-15 15:39:23 -0500

    adding new files

--- packages/the_meal_db_api_client/lib/src/the_meal_db_api_client.dart
+++ packages/the_meal_db_api_client/lib/src/the_meal_db_api_client.dart
@@ -1,25 +1,11 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-/// {@template the_meal_db_api_client}
-/// meal_db_api
-/// {@endtemplate}
-/// import 'dart:convert';
-///
+// ignore_for_file: public_member_api_docs
 
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:the_meal_db_api_client/src/models/meal.dart';
 import 'package:the_meal_db_api_client/src/models/meals.dart';
 
-// ignore: public_member_api_docs
 class TheMealDbApiClient {
-  // ignore: public_member_api_docs
   TheMealDbApiClient({
     this.mealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php',
     required this.client,
@@ -28,7 +14,7 @@ class TheMealDbApiClient {
   final String mealUrl;
   final http.Client client;
 
-  Future<Meal> fetchRandomMeal() async {
+  Future<Meals> fetchRandomMeal() async {
     final response = await client.get(Uri.parse(mealUrl));
 
     if (response.statusCode == 200) {
